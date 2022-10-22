9db9690008f523ebe5e46fdb89e10de57f3d6ec5
annam <choirul.annam@zettacamp.pro>
2022-10-22 23:44:37 +0700

    create avatar component

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avatar.component.css']
 })
 export class AvatarComponent implements OnInit {
+  // buat nangkep url gambarnya
+  @Input() src!: string;
 
   constructor() { }
 
