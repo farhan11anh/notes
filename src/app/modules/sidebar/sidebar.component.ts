87647aa45f959e750529e99e1f04d4117aac07b4
mcanam <mcanam910@gmail.com>
2022-10-23 09:13:39 +0700

    update sidebar component

--- src/app/modules/sidebar/sidebar.component.ts
+++ src/app/modules/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  @Input() open: boolean;
 
-  constructor() { }
+  constructor(
+    private element: ElementRef
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges() {
+    const elem = this.element.nativeElement;
+    elem.classList[this.open ? 'add' : 'remove']('open');
   }
 
 }
--- src/app/modules/sidebar/sidebar.module.ts
+++ src/app/modules/sidebar/sidebar.module.ts
@@ -4,13 +4,14 @@ import { NavContainerComponent } from './nav-container/nav-container.component';
 import { NavItemComponent } from './nav-item/nav-item.component';
 import { SidebarComponent } from './sidebar.component';
 
-
+import { IconComponent } from 'src/app/components/icon/icon.component';
 
 @NgModule({
   declarations: [
     NavContainerComponent,
     NavItemComponent,
-    SidebarComponent
+    SidebarComponent,
+    IconComponent
   ],
   imports: [
     CommonModule
